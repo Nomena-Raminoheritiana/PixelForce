819d152f33427917935008c5840fa166b40a9ad9
Nomena <n.raminoheritiana@gmail.com>
2022-05-29 20:05:55 +0100

    fix dropdown navbar header (compatibilité entre bootrsatp installé normalement et le bootstrap dans vuexy)

--- assets/app.js
+++ assets/app.js
@@ -15,4 +15,25 @@ import { Tooltip, Toast, Popover, Collapse, Modal } from 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
+$(document).ready(function() {
+    // dropdown bootstrap fix
+    let dropdown = false;
+    $(this).on('click', '[data-bs-toggle="dropdown"]', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        const idDrop = $(this).attr('id');
+        if(idDrop) {
+            $(this).closest('.dropdown').find('[aria-labelledby="'+idDrop+'"]').first().toggleClass('show');
+        } else {
+            $(this).next('.dropdown-menu').first().toggleClass('show')
+        }
+
+        dropdown = true;
+    }).on('click', 'body', function() {
+        $('.dropdown-menu').each(function() {
+            $(this).removeClass('show');
+        })
+    });
+});
+
 require('./js/speedLiveVideo');
