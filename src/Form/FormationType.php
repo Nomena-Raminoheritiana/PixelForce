8154e5a3deb041df6ae9938de6ed9b313c38785f
Nomena <n.raminoheritiana@gmail.com>
2022-05-30 20:52:36 +0100

    ajout champ debloqueAgent (entity Formation)

---
+++ src/Form/FormationType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('description_deblocage')
+            ->add('contenu')
+            ->add('lien_video')
+            ->add('estDisponible')
+            ->add('estDebloquee')
+            ->add('brouillon')
+            ->add('coach')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
