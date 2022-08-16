97bbc1bda0c3721930f5b067d5e0000c9128b8a7
Tsiory <fordevproject@gmail.com>
2022-08-16 16:01:37 +0300

    Activation Mercure/Messenger (Besoin en prod)

--- assets/js/chat/chatMercureTraitement.js
+++ assets/js/chat/chatMercureTraitement.js
@@ -3,58 +3,58 @@ import {showMessageInstance, updateMenu} from "./helpers/chat_helpers";
 
 $(function() {
     // detecter s'il y a un message
-    // const chat_newMessage_topic = JSON.parse(document.getElementById("chat-newMessage-topic").textContent);
-    // const chat_vu_topic = JSON.parse(document.getElementById("chat-vu-topic").textContent);
-    // const chat_userTyping_topic = JSON.parse(document.getElementById("chat-userTyping-topic").textContent);
-    // const chat_userStopTyping_topic = JSON.parse(document.getElementById("chat-userStopTyping-topic").textContent);
-    // const eventNewMessage = new EventSource(chat_newMessage_topic);
-    // const eventVu = new EventSource(chat_vu_topic);
-    // const eventUserTyping = new EventSource(chat_userTyping_topic);
-    // const eventUserStopTyping = new EventSource(chat_userStopTyping_topic);
-    // const conversationComponent = new ConversationComponent();
+    const chat_newMessage_topic = JSON.parse(document.getElementById("chat-newMessage-topic").textContent);
+    const chat_vu_topic = JSON.parse(document.getElementById("chat-vu-topic").textContent);
+    const chat_userTyping_topic = JSON.parse(document.getElementById("chat-userTyping-topic").textContent);
+    const chat_userStopTyping_topic = JSON.parse(document.getElementById("chat-userStopTyping-topic").textContent);
+    const eventNewMessage = new EventSource(chat_newMessage_topic);
+    const eventVu = new EventSource(chat_vu_topic);
+    const eventUserTyping = new EventSource(chat_userTyping_topic);
+    const eventUserStopTyping = new EventSource(chat_userStopTyping_topic);
+    const conversationComponent = new ConversationComponent();
     // présence d'un nouveau message
-    // eventNewMessage.onmessage = async event => {
-    //     const message = JSON.parse(event.data);
-    //     const canal = message.canal;
-    //     const user = message.user;
-    //     // si une instance est ouverte
-    //     let jCanal = $('#canal-'+canal.id);
-    //     jCanal.find('.chat-list-group-messages').append(conversationComponent.getMessage(message));
-    //     // si l'instance n'est pas ouverte
-    //     if(jCanal.length === 0 ) {
-    //       jCanal = await showMessageInstance(canal);
-    //     }
-    //     // mettre en évidence le canal
-    //     conversationComponent.addHighligh(jCanal)
-    //     // mettre à jour le menu
-    //     await updateMenu(canal)
-    // }
+    eventNewMessage.onmessage = async event => {
+        const message = JSON.parse(event.data);
+        const canal = message.canal;
+        const user = message.user;
+        // si une instance est ouverte
+        let jCanal = $('#canal-'+canal.id);
+        jCanal.find('.chat-list-group-messages').append(conversationComponent.getMessage(message));
+        // si l'instance n'est pas ouverte
+        if(jCanal.length === 0 ) {
+          jCanal = await showMessageInstance(canal);
+        }
+        // mettre en évidence le canal
+        conversationComponent.addHighligh(jCanal)
+        // mettre à jour le menu
+        await updateMenu(canal)
+    }
 
     // quand quelqu'un voit le message
-    // eventVu.onmessage = async event => {
-    //     const canal = JSON.parse(event.data);
-    //     const dernierMessageItem = $('.chat-list-group-messages>.list-group-item').last();
-    //     const idUserEnvoyeur = parseInt(dernierMessageItem.attr('data-id-user'))
-    //     // si une instance est ouverte et que la personne qui a vu le message n'est pas l'envoyeur
-    //     let jCanal = $('#canal-'+canal.id);
-    //     conversationComponent.clearVu(jCanal)
-    //     if(jCanal.length > 0 && idUserEnvoyeur !== canal.vuPar.user.id ) {
-    //        // todo: mettre un vu
-    //         conversationComponent.addVu(jCanal, canal);
-    //     }
-    // }
+    eventVu.onmessage = async event => {
+        const canal = JSON.parse(event.data);
+        const dernierMessageItem = $('.chat-list-group-messages>.list-group-item').last();
+        const idUserEnvoyeur = parseInt(dernierMessageItem.attr('data-id-user'))
+        // si une instance est ouverte et que la personne qui a vu le message n'est pas l'envoyeur
+        let jCanal = $('#canal-'+canal.id);
+        conversationComponent.clearVu(jCanal)
+        if(jCanal.length > 0 && idUserEnvoyeur !== canal.vuPar.user.id ) {
+           // todo: mettre un vu
+            conversationComponent.addVu(jCanal, canal);
+        }
+    }
 
     // quand quelqu'un est en train d'écrire
-    // eventUserTyping.onmessage = async event => {
-    //     const canal = JSON.parse(event.data);
-    //     conversationComponent.addUserTyping($('.chat-box-container[id="canal-'+canal.id+'"]'), canal.userTyping)
-    // }
-    //
-    // // quand quelqu'un arrete d'écrire
-    // eventUserStopTyping.onmessage = async event => {
-    //     const canal = JSON.parse(event.data);
-    //     conversationComponent.removeUserTyping($('.chat-box-container[id="canal-'+canal.id+'"]'), canal.userTyping)
-    // }
+    eventUserTyping.onmessage = async event => {
+        const canal = JSON.parse(event.data);
+        conversationComponent.addUserTyping($('.chat-box-container[id="canal-'+canal.id+'"]'), canal.userTyping)
+    }
+    
+    // quand quelqu'un arrete d'écrire
+    eventUserStopTyping.onmessage = async event => {
+        const canal = JSON.parse(event.data);
+        conversationComponent.removeUserTyping($('.chat-box-container[id="canal-'+canal.id+'"]'), canal.userTyping)
+    }
 
 
 });
--- src/Services/Chat/ChatMercureNotification.php
+++ src/Services/Chat/ChatMercureNotification.php
@@ -34,116 +34,115 @@ class ChatMercureNotification
         $this->chatNormalizer = $chatNormalizer;
         $this->hub = $hub;
     }
-/***** désactiver temporairement les fonctionnalités pour besoin en production ****/
+    
     public function notifyWhenNewMessage(Message $message)
     {
         // notifier tous les membres abonné au canal
-        // désactiver temporairement la fonctionnalité pour besoin en production
-//        $canalMessage = $message->getCanalMessage();
-//        $users = $canalMessage->getUsers()->toArray();
-//        foreach($users as $user) {
-//            if($user->getId() != $message->getUser()->getId()) {
-//                $encodedIdUser = base64_encode($user->getId());
-//                $update = new Update(
-//                    self::CHAT_ADD_MESSAGE_TOPIC.$encodedIdUser,
-//                    json_encode($this->chatNormalizer->getMessageNormalized($message))
-//                );
-//                $this->hub->publish($update);
-//            }
-//        }
+       $canalMessage = $message->getCanalMessage();
+       $users = $canalMessage->getUsers()->toArray();
+       foreach($users as $user) {
+           if($user->getId() != $message->getUser()->getId()) {
+               $encodedIdUser = base64_encode($user->getId());
+               $update = new Update(
+                   self::CHAT_ADD_MESSAGE_TOPIC.$encodedIdUser,
+                   json_encode($this->chatNormalizer->getMessageNormalized($message))
+               );
+               $this->hub->publish($update);
+           }
+       }
 
     }
 
     public function notifyWhenNewCanalMessage(CanalMessage $canal)
     {
         // notifier tous les membres abonné au canal
-//        $users = $canal->getUsers()->toArray();
-//        foreach($users as $user) {
-//            $encodedIdUser = base64_encode($user->getId());
-//            $update = new Update(
-//                self::CHAT_ADD_CANAL_TOPIC.$encodedIdUser,
-//                json_encode( json_encode($this->chatNormalizer->getCanalMessageNormalized($canal)))
-//            );
-//            $this->hub->publish($update);
-//        }
+       $users = $canal->getUsers()->toArray();
+       foreach($users as $user) {
+           $encodedIdUser = base64_encode($user->getId());
+           $update = new Update(
+               self::CHAT_ADD_CANAL_TOPIC.$encodedIdUser,
+               json_encode( json_encode($this->chatNormalizer->getCanalMessageNormalized($canal)))
+           );
+           $this->hub->publish($update);
+       }
     }
 
     public function notifyWhenNewVu(CanalMessage $canalMessage, User $userEmitter)
     {
         // notifier tous les membres abonné au canal
-//        $users = $canalMessage->getUsers()->toArray();
-//        foreach($users as $user) {
-//            $encodedIdUser = base64_encode($user->getId());
-//            $update = new Update(
-//                self::CHAT_ADD_VU_TOPIC.$encodedIdUser,
-//                json_encode(array_merge($this->chatNormalizer->getCanalMessageNormalized($canalMessage), ['vuPar' => [
-//                    'user' => [
-//                        'id' => $userEmitter->getId(),
-//                        'nom' => $userEmitter->getNom(),
-//                        'prenom' => $userEmitter->getPrenom()
-//                    ]
-//                ]]))
-//            );
-//            $this->hub->publish($update);
-//        }
+       $users = $canalMessage->getUsers()->toArray();
+       foreach($users as $user) {
+           $encodedIdUser = base64_encode($user->getId());
+           $update = new Update(
+               self::CHAT_ADD_VU_TOPIC.$encodedIdUser,
+               json_encode(array_merge($this->chatNormalizer->getCanalMessageNormalized($canalMessage), ['vuPar' => [
+                   'user' => [
+                       'id' => $userEmitter->getId(),
+                       'nom' => $userEmitter->getNom(),
+                       'prenom' => $userEmitter->getPrenom()
+                   ]
+               ]]))
+           );
+           $this->hub->publish($update);
+       }
     }
 
     public function notifyWhenAddUser(CanalMessage $canalMessage, User $user)
     {
-//        $encodedIdUser = base64_encode($user->getId());
-//        $update = new Update(
-//            self::CHAT_ADD_USER_TOPIC.$encodedIdUser,
-//            json_encode($this->chatNormalizer->getCanalMessageNormalized($canalMessage))
-//        );
-//        $this->hub->publish($update);
+       $encodedIdUser = base64_encode($user->getId());
+       $update = new Update(
+           self::CHAT_ADD_USER_TOPIC.$encodedIdUser,
+           json_encode($this->chatNormalizer->getCanalMessageNormalized($canalMessage))
+       );
+       $this->hub->publish($update);
     }
 
     public function notifyWhenRemoveUser(CanalMessage $canalMessage, User $user)
     {
-//        $encodedIdUser = base64_encode($user->getId());
-//        $update = new Update(
-//            self::CHAT_REMOVE_USER_TOPIC.$encodedIdUser,
-//            json_encode($this->chatNormalizer->getCanalMessageNormalized($canalMessage))
-//        );
-//        $this->hub->publish($update);
+       $encodedIdUser = base64_encode($user->getId());
+       $update = new Update(
+           self::CHAT_REMOVE_USER_TOPIC.$encodedIdUser,
+           json_encode($this->chatNormalizer->getCanalMessageNormalized($canalMessage))
+       );
+       $this->hub->publish($update);
     }
 
     public function notifyWhenRemoveCanal(CanalMessage $canalMessage)
     {
         // notifier tous les membres abonné au canal
-//        $users = $canalMessage->getUsers()->toArray();
-//        foreach($users as $user) {
-//            $encodedIdUser = base64_encode($user->getId());
-//            $update = new Update(
-//                self::CHAT_REMOVE_CANAL_TOPIC.$encodedIdUser,
-//                json_encode($this->chatNormalizer->getCanalMessageNormalized($canalMessage))
-//            );
-//            $this->hub->publish($update);
-//        }
+       $users = $canalMessage->getUsers()->toArray();
+       foreach($users as $user) {
+           $encodedIdUser = base64_encode($user->getId());
+           $update = new Update(
+               self::CHAT_REMOVE_CANAL_TOPIC.$encodedIdUser,
+               json_encode($this->chatNormalizer->getCanalMessageNormalized($canalMessage))
+           );
+           $this->hub->publish($update);
+       }
     }
 
     public function notifyWhenUserIsTyping(User $userTyping, CanalMessage $canalMessage, $stop = false)
     {
         // notifier tous les membres abonné au canal
-//        $users = $canalMessage->getUsers()->toArray();
-//        foreach($users as $user) {
-//            if($user->getId() != $userTyping->getId()) {
-//                $encodedIdUser = base64_encode($user->getId());
-//                $update = new Update(
-//                    !$stop ? self::CHAT_USER_TYPING_TOPIC.$encodedIdUser : self::CHAT_USER_STOP_TYPING_TOPIC.$encodedIdUser,
-//                    json_encode(array_merge($this->chatNormalizer->getCanalMessageNormalized($canalMessage),
-//                        [
-//                            'userTyping' => [
-//                                'id' => $userTyping->getId(),
-//                                'nom' => $userTyping->getNom(),
-//                                'prenom' => $userTyping->getPrenom()
-//                            ]
-//                        ]
-//                    ))
-//                );
-//                $this->hub->publish($update);
-//            }
-//        }
+       $users = $canalMessage->getUsers()->toArray();
+       foreach($users as $user) {
+           if($user->getId() != $userTyping->getId()) {
+               $encodedIdUser = base64_encode($user->getId());
+               $update = new Update(
+                   !$stop ? self::CHAT_USER_TYPING_TOPIC.$encodedIdUser : self::CHAT_USER_STOP_TYPING_TOPIC.$encodedIdUser,
+                   json_encode(array_merge($this->chatNormalizer->getCanalMessageNormalized($canalMessage),
+                       [
+                           'userTyping' => [
+                               'id' => $userTyping->getId(),
+                               'nom' => $userTyping->getNom(),
+                               'prenom' => $userTyping->getPrenom()
+                           ]
+                       ]
+                   ))
+               );
+               $this->hub->publish($update);
+           }
+       }
     }
 
 }
