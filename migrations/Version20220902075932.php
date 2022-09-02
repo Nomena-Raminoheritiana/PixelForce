80ee473daf6633494739f6e929bff935cc9276db
MC-Michel <58548847+MC-Michel@users.noreply.github.com>
2022-09-02 11:04:15 +0300

    Update Version20220902075932.php

--- migrations/Version20220902075932.php
+++ migrations/Version20220902075932.php
@@ -20,10 +20,10 @@ final class Version20220902075932 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE devis (id INT AUTO_INCREMENT NOT NULL, demande_devis_id INT NOT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT DEFAULT NULL, price DOUBLE PRECISION NOT NULL, files JSON DEFAULT NULL, created_at DATETIME NOT NULL, status VARCHAR(100) NOT NULL, contrat_file_name VARCHAR(255) DEFAULT NULL, status_int INT DEFAULT NULL, INDEX IDX_8B27C52B787B318 (demande_devis_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        //$this->addSql('CREATE TABLE devis (id INT AUTO_INCREMENT NOT NULL, demande_devis_id INT NOT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT DEFAULT NULL, price DOUBLE PRECISION NOT NULL, files JSON DEFAULT NULL, created_at DATETIME NOT NULL, status VARCHAR(100) NOT NULL, contrat_file_name VARCHAR(255) DEFAULT NULL, status_int INT DEFAULT NULL, INDEX IDX_8B27C52B787B318 (demande_devis_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE order_digital (id INT AUTO_INCREMENT NOT NULL, agent_id INT DEFAULT NULL, client_id INT DEFAULT NULL, devis_id INT NOT NULL, amount DOUBLE PRECISION NOT NULL, created_at DATETIME NOT NULL, stripe_data LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', agent_email VARCHAR(255) NOT NULL, client_email VARCHAR(255) NOT NULL, stripe_charge_id VARCHAR(255) NOT NULL, INDEX IDX_3D32E49C3414710B (agent_id), INDEX IDX_3D32E49C19EB6921 (client_id), UNIQUE INDEX UNIQ_3D32E49C41DEFADA (devis_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         //$this->addSql('CREATE TABLE produit_qte_stock (id INT AUTO_INCREMENT NOT NULL, produit_id INT NOT NULL, qte_stock DOUBLE PRECISION NOT NULL, UNIQUE INDEX UNIQ_41E7AA3F347EFB (produit_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE devis ADD CONSTRAINT FK_8B27C52B787B318 FOREIGN KEY (demande_devis_id) REFERENCES demande_devis (id)');
+        //$this->addSql('ALTER TABLE devis ADD CONSTRAINT FK_8B27C52B787B318 FOREIGN KEY (demande_devis_id) REFERENCES demande_devis (id)');
         $this->addSql('ALTER TABLE order_digital ADD CONSTRAINT FK_3D32E49C3414710B FOREIGN KEY (agent_id) REFERENCES `user` (id)');
         $this->addSql('ALTER TABLE order_digital ADD CONSTRAINT FK_3D32E49C19EB6921 FOREIGN KEY (client_id) REFERENCES `user` (id)');
         $this->addSql('ALTER TABLE order_digital ADD CONSTRAINT FK_3D32E49C41DEFADA FOREIGN KEY (devis_id) REFERENCES devis (id)');
@@ -37,7 +37,7 @@ final class Version20220902075932 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE order_digital DROP FOREIGN KEY FK_3D32E49C41DEFADA');
-        $this->addSql('DROP TABLE devis');
+        //$this->addSql('DROP TABLE devis');
         $this->addSql('DROP TABLE order_digital');
         //$this->addSql('DROP TABLE produit_qte_stock');
         $this->addSql('ALTER TABLE demande_devis CHANGE files files LONGTEXT DEFAULT NULL COLLATE `utf8mb4_bin`');
